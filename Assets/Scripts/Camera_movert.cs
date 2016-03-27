a8c465d0d55b37f0b57a546c483193dccb528adf
Mirosław Pucka <Iluzion92@wp.pl>
2016-03-27 19:53:23 +0200

    Gra szukajka, zrobiony zarys mapy i kulka z kamerą w 1 osobie. Dodano 1 postać i 2 zestawy tekstur ze strony unity.

---
+++ Assets/Scripts/Camera_movert.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_movert : MonoBehaviour {
+
+	public GameObject Player;
+	private Vector3 offset;
+
+	void Start () {
+		transform.position = Player.transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Kostka.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Kostka : MonoBehaviour {
+
+	Rigidbody rBody;
+	public float objectSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Funkcja Start");
+		rBody = GetComponent <Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate () {
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		Debug.Log ("h: " +horizontal+ "v: " +vertical+ "time " +Time.deltaTime);
+		rBody.AddForce (new Vector3(horizontal,0,vertical) * objectSpeed);
+	}
+}
